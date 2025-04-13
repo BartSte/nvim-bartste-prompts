f1563fb7b1b0f8c51579434fd27552532cfd7ab2
barts <bartsteensma@outlook.com>
2025-04-13 21:01:04 +0200

    refactor: simplify command execution and add locking mechanism

--- lua/prompts/commands.lua
+++ lua/prompts/commands.lua
@@ -1,25 +1,21 @@
 local M = {}
 
---- Reloads the current buffer from disk without prompting
----@param file string The path of the file to reload
-local function reload_buffer(file)
-  vim.cmd("silent! edit! " .. file)
-end
+local lock = ''
 
 --- Creates a command function that runs a given command on the current buffer file.
 ---@param command string The command to run.
 ---@return function A function without parameters that executes the command.
 function M.make(command)
   return function()
-    local file = vim.api.nvim_buf_get_name(0)
-    local filetype = vim.bo.filetype
-    M.run(command, file, filetype)
-    reload_buffer(file)
+    M.run(command)
   end
 end
 
---- Callback function executed when a system command exits.
----@param obj table The exit information with fields: code (number), stdout (string), stderr (string)
+--- Callback handler for command execution completion
+---@param obj {code: number, stdout: string, stderr: string} Table containing exit status and outputs
+---@field code number Exit status code (0 indicates success)
+---@field stdout string Standard output content
+---@field stderr string Standard error content
 local function on_exit(obj)
   if obj.code ~= 0 then
     vim.notify(string.format("Command failed with exit code: %d", obj.code), vim.log.levels.ERROR)
@@ -28,17 +24,31 @@ local function on_exit(obj)
   else
     vim.notify("Command succeeded", vim.log.levels.INFO)
   end
+  lock = ''
 end
 
+local function run(command)
+  if lock ~= '' then
+    vim.notify(string.format("Another command '%s' is already running", lock), vim.log.levels.WARN)
+    return
+  end
 
---- Executes a system command using prompts-aider with the given parameters.
----@param command string The command identifier.
----@param file string The current file name.
----@param ft string The filetype.
-function M.run(command, file, ft)
-  local cmd = { "prompts", command, file, "--filetype", ft, "--action", "aider" }
+  lock = command
+  local file = vim.api.nvim_buf_get_name(0)
+  local filetype = vim.bo.filetype
+  local cmd = { "prompts", command, file, "--filetype", filetype, "--action", "aider" }
   vim.notify(string.format("Running: %s", table.concat(cmd, " ")))
   vim.system(cmd, {}, on_exit)
+end
+
+--- Safely execute a command with error protection
+--- @param command string The identifier of the command to run
+function M.run(command)
+  local ok, _ = pcall(run, command)
+  if not ok then
+    vim.notify(string.format("Command %s failed", command), vim.log.levels.ERROR)
+    lock = ''
+  end
 end
 
 return M
