9ba01c3d74d5dce88a9a166644cf21656a834151
barts <bartsteensma@outlook.com>
2025-04-14 20:09:45 +0200

    refactor: wrap command exit handler in vim.schedule_wrap

--- lua/prompts/commands.lua
+++ lua/prompts/commands.lua
@@ -2,29 +2,18 @@ local M = {}
 
 local lock = ''
 
---- Creates a command function that runs a given command on the current buffer file.
----@param command string The command to run.
----@return function A function without parameters that executes the command.
-function M.make(command)
-  return function()
-    M.run(command)
-  end
-end
-
---- Handle command completion results and notify user
----@param obj table Command exit information containing:
----@field code number Exit status code (0 for success)
----@field stdout string Captured standard output
----@field stderr string Captured standard error
-local function on_exit(obj)
-  if obj.code ~= 0 then
-    vim.notify(string.format("Command failed with exit code: %d", obj.code), vim.log.levels.ERROR)
-    vim.notify(string.format("stderr: %s", obj.stderr), vim.log.levels.ERROR)
-    vim.notify(string.format("stdout: %s", obj.stdout), vim.log.levels.INFO)
-  else
-    vim.notify("Command succeeded", vim.log.levels.INFO)
-  end
-  lock = ''
+local function schedule_on_exit(file)
+  return vim.schedule_wrap(function(obj)
+    if obj.code ~= 0 then
+      vim.notify(string.format("Command failed with exit code: %d", obj.code), vim.log.levels.ERROR)
+      vim.notify(string.format("stderr: %s", obj.stderr), vim.log.levels.ERROR)
+      vim.notify(string.format("stdout: %s", obj.stdout), vim.log.levels.INFO)
+    else
+      vim.notify("Command succeeded", vim.log.levels.INFO)
+      vim.cmd("edit " .. file)
+    end
+    lock = ''
+  end)
 end
 
 --- Execute a system command for the current buffer with concurrency control
@@ -41,7 +30,7 @@ local function run(command)
   local filetype = vim.bo.filetype
   local cmd = { "prompts", command, file, "--filetype", filetype, "--action", "aider" }
   vim.notify(string.format("Running: %s", table.concat(cmd, " ")))
-  vim.system(cmd, {}, on_exit)
+  vim.system(cmd, {}, schedule_on_exit(file))
 end
 
 --- Safely execute a command with error protection
@@ -51,6 +40,15 @@ function M.run(command)
   if not ok then
     vim.notify(string.format("Command %s failed", command), vim.log.levels.ERROR)
     lock = ''
+  end
+end
+
+--- Creates a command function that runs a given command on the current buffer file.
+---@param command string The command to run.
+---@return function A function without parameters that executes the command.
+function M.make(command)
+  return function()
+    M.run(command)
   end
 end
 
